16f7c1dacf8d8c4014cbb9a084c136e6b253227e
Liu, Tongxin <92342283+tl3103@users.noreply.github.com>
2021-11-15 20:01:51 -0500

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -15,3 +15,10 @@ and has a second method called cost which returns a float, the total cost paid f
 Consider that to implement the cost method, you'll need to be storing the purchases made each time the buy method is called somewhere. You may do so by any means convenient to you.
 
 Commit this file to your repository and push it to GitHub using GitHub Desktop, with a suitable commit message.'''
+
+class Portfolio:
+    def __init__(self, contacts):
+        self.contacts = contacts
+    def buy(self, name, shares, price):
+        book.concats()
+    

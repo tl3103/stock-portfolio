fc5d96799bc16b1131d83026ed29e6a5b6ccefa5
Liu, Tongxin <92342283+tl3103@users.noreply.github.com>
2021-11-15 21:03:50 -0500

    Update test_portfolio.py

--- test_portfolio.py
+++ test_portfolio.py
@@ -1,6 +1,10 @@
 # test_portfolio.py
 from portfolio import Portfolio
 
+def test_empty_portfolio():
+    p = Portfolio()
+    assert p.cost() == 0.0
+
 def test_buy_one_stock():
     p = Portfolio()
     p.buy("IBM", 100, 176.48)

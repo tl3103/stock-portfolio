e211e4baccea8d1b156079de533f885f266bda43
Liu, Tongxin <92342283+tl3103@users.noreply.github.com>
2021-11-15 21:14:12 -0500

    Update test_portfolio.py

--- test_portfolio.py
+++ test_portfolio.py
@@ -13,5 +13,9 @@ def test_buy_one_stock():
 def test_buy_two_stocks():
     p = Portfolio()
     p.buy('IBM', 100, 176.48)
-    p.buy('HPQ',100, 36.15)
+    p.buy('HPQ', 100, 36.15)
     assert p.cost() == 21263.00
+
+def test_not_enough_arguments_to_but():
+    p = Portfolio()
+    p.buy('IBM')
